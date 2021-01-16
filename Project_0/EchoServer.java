5dab558d7d92daa5676033238f5ff5b36d741ba3
Logan Behnke <lmb548@nau.edu>
2021-01-16 01:44:33 -0700

    multi-threaded the server and echos char by char

--- Project_0/EchoServer.java
+++ Project_0/EchoServer.java
@@ -1,81 +1,51 @@
 import java.io.*;
 import java.net.*;
 
-public class EchoServer
-{
+public class EchoServer {
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         System.out.println("Echo Server is started!");
-        try 
-        (ServerSocket echoServer = new ServerSocket(8000);)
-        {
-            Socket clientSocket = echoServer.accept();
-            System.out.println("We are connected to client");
-            BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(),true);
-            String line;
-            //take in characeter by character
-           // char charFromClient;
-           // char[] charArray = new char[30];
-
-            while((line = fromClient.readLine()) != null)
-            {
-                System.out.println("On Server: " + line);
-                toClient.println(line);
+        try (ServerSocket echoServer = new ServerSocket(8000);) {
+            while (true) {
+                Socket clientSocket = echoServer.accept();
+                new Thread(new EchoThread(clientSocket)).start();
             }
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 }
 
-//Please ignore, this is a work in progress
-/*
-class EchoThread implements Runnable
-{
+class EchoThread implements Runnable {
     private Socket socket;
-    private in count;
     private long threadId;
-    InputStream fromClient;
-    OutputStream toClient;
-    private PrintWriter pw;
+    private BufferedReader fromClient;
+    private char charFromClient;
+    private PrintWriter toClient;
 
-    EchoThread(Sockets socket, int count)
-    {
+    EchoThread(Socket socket) {
         this.socket = socket;
-        this.count = count;
     }
 
-    public long getThreadId()
-    {
+    public long getThreadId() {
         threadId = Thread.currentThread().getId();
-        return id;
+        return threadId;
     }
 
-    @Ovveride
-    public void run()
-    {
-        try
-        {
-            fromClient = socket.getInputStream();
-            toClient = socket.getOutputStream();
-        }
-
-        catch(IOException e)
-        {
-            System.err.println(e);
-        }
-
-        try(Scanner sc = new Scanner(fromClient))
-        {
-            pw = new PrintWriter(toClient, true);
-
-            if()
+    @Override
+    public void run() {
+        try {
+            int i;
+            // sets up the input and output streams
+            fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            toClient = new PrintWriter(socket.getOutputStream(), true);
+            while ((i = fromClient.read()) != -1) {
+                charFromClient = (char) i; // converts an int to a char
+                toClient.println(charFromClient);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
-*/
